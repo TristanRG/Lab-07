5aa73d6e16932c577cf53f906abc757a35f650b5
TristanRG <115103112+TristanRG@users.noreply.github.com>
2022-11-20 21:06:19 +0200

    Update Vector.java

--- src/Vector.java
+++ src/Vector.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Vector {
+import java.util.Scanner;
+public class Vector {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Introdu cate n numere = >>> ");
+        int n = input.nextInt();
+        int v[] = new int[n];
+        int i;
+        for(i = 0; i < n; i++) {
+            System.out.println("V[" + i + "] = ");
+            v[i] = input.nextInt();
+        }
+
+        int min;
+        for(i = 0; i < n; i++) {
+            min = 10000;
+            if(min > v[i]) min = v[i];
+            System.out.println("Minimul vectorului " + i + " este egal cu " + min);
+        }
+    }
 }
